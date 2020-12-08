b38482d80080544e746cdb282b6807a80577ad9b
Michael Eclavea <usermike671@gmail.com>
2020-12-07 21:19:11 -0800

    added two pages and button

--- App.js
+++ App.js
@@ -1,14 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, Button, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+
+
+const Stack = createStackNavigator();
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Michael's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Michael' })
+      }
+    />
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return (
+    <>
+  <Text>This is {route.params.name}'s profile</Text>
+      <View style={styles.container}>
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
+    </View>
+    </>
+  );
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +59,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
   },
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# REACT-NATIVE IPHONE APP
+
+Using the React-Native framework, we have implemented a starter code app. It contains two functionalities 
+a homepage and a profile page. There is also a click button that once pressed displays a text on screen. 
+
+
+### Install instructions.
+when forking repo, npm i for all dependencies. 
+install expo on your phone
+npm run in terminal 
+scan barcode from phone 
+
+!UML](./uml/image.jpeg).
